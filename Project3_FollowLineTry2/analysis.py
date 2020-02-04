dbf817be1d0ec9760b4ad19c33a626ecd18792e7
Priya Misner <priya.misner@tufts.edu>
2020-02-03 21:50:37 -0500

    analysis script for graphing

---
+++ Project3_FollowLineTry2/analysis.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+filename ='./error.csv'
+fin = open(filename,'r')
+data = []
+for line in fin:    
+    line = line.split(',')
+    print(line)
+    data.append(line)
+
+df= pd.DataFrame(data)
+
+time = np.arange(53)
+y1 = pd.to_numeric(df[1])
+y2 =pd.to_numeric(df[3])
+plt.scatter(time,y2)
+plt.scatter(time,y1)
+
+plt.ylim((0,max(y2)+50))
