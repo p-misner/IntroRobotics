fe61e12165e03973ce6e1fba4f5dd1b5cfc78931
Priya Misner <priya.misner@tufts.edu>
2020-02-10 19:40:05 -0500

    proj 4

--- Project4_Blind/Project4_ImagesforBlind/main.py
+++ Project4_Blind/Project4_ImagesforBlind/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+#brickrun -r -- pybricks-micropython
 
 from pybricks import ev3brick as brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
@@ -7,42 +8,61 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
-'''from pybricks.ev3devio import Ev3devSensor 
-import utime
-import ev3dev2
-from ev3dev2.port import LegoPort '''
+from pybricks.iodevices import AnalogSensor, UARTDevice
 
-# Write your program here
 brick.sound.beep()
 
-filename = 'error.csv'
-fobj = open(filename, 'w')
-
-#stationary = Motor(Port.D, Direction.CLOCKWISE)
-#rotating = Motor(Port.B, Direction.CLOCKWISE)
-
-#right_motor = Motor(Port.C, Direction.CLOCKWISE)
-#left_motor = Motor(Port.A, Direction.CLOCKWISE)
-minimotor = Motor(Port.A, Direction.CLOCKWISE)
+minimotor = Motor(Port.D)
+leftmotor = Motor(Port.C, Direction.CLOCKWISE)
+rightmotor= Motor(Port.B, Direction.CLOCKWISE)
+robot = DriveBase(leftmotor,rightmotor, 40, 200)
 button = TouchSensor(Port.S1)
-#color = ColorSensor(Port.S4)
-#robot = DriveBase(right_motor, left_motor,  )
-direction =1
-while True:
-    #print(color.rgb())
-    if button.pressed():
-        direction = direction *-1
-    minimotor.run(direction*40)
-    #left_motor.run(direction*0)
-'''
-while True:
-    sangle = stationary.angle() 
-    rangle = rotating.angle()
-    while button.pressed():
-        print('hi')
-        rotating.run_target(1,1, stop_type=Stop.BRAKE)
-    rotating.run_time(1,1, stop_type=Stop.COAST)
-    fobj.write('Rotating,'+str(rangle)+ ',Stationary,'+ str(sangle)+'\n')
-'''
+color = ColorSensor(Port.S2)
 
-fobj.close()
+#sense = AnalogSensor(Port.S3, False)
+
+
+direction = 1
+
+while True:
+    if button.pressed():
+        direction *= -1
+    minimotor.run(direction*20)
+    robot.drive(direction*0,0)
+    data = color.color()
+    #uart.write(data)
+    wait(500)
+    #print(color.color())
+
+
+
+
+
+
+
+'''
+from pybricks.hubs import EV3Brick
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.parameters import Color, Port
+from pybricks.ev3devices import Motor
+from pybricks.iodevices import AnalogSensor, UARTDevice
+
+# Initialize the EV3
+ev3 = EV3Brick()
+ev3.speaker.beep()
+sense = AnalogSensor(Port.S3, False)
+uart = UARTDevice(Port.S3, 9600, timeout=2000)
+
+
+#watch = StopWatch()
+
+# Turn on a red light
+ev3.light.on(Color.BLUE)
+ev3.speaker.say("About to take data")
+count = 0
+while True:
+    wait(1000)
+    data = uart.read_all()  #if you connect Pin 5 & 6 you should see Test on the screen
+    count += 1
+    ev3.screen.print(count,data)
+'''
