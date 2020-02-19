e052dcb38d9060ba2e2e1c62da65f4a884e136c6
Priya Misner <priya.misner@tufts.edu>
2020-02-19 15:01:20 -0500

    recent update, drums

--- Project5_MagicConductor/main.py
+++ Project5_MagicConductor/main.py
@@ -14,9 +14,8 @@ import math
 
 # Write your program here
 ev3 = EV3Brick()
-ev3.speaker.beep()
 #ev3.speaker.say('start')
-#brick.sound.beep(500,1000,20)
+brick.sound.beep(500,1000,20)
 tare = TouchSensor(Port.S1)
 wait(100)
 
@@ -45,49 +44,34 @@ def map(val, mode):
         val = (val/1000)*90
         return val
 
+def drum(roll):
+    roll +=270
+    begin = [0,120,240]
+    end = [120,240,360]
+    files = ['AcousticHat-06.wav','AcousticSnare-04.wav','cymbal-01.wav']
+    for i in range(len(begin)):
+        if roll >= begin[i] and roll <end[i]:
+            return(files[i])
+       
 def main():
     s = Serial("/dev/ttyACM0", 9600)
+    s2 = Serial("/dev/ttyACM1", 9600)
     iter = 0
     labels = ['xAcc', 'yAcc', 'zAcc', 'xGyro', 'yGyro', 'zGyro']
     x_gyro, y_gyro, z_gyro = 0, 0, 0
-    '''
-    while not tare.pressed():
-        # Read raw data
-        raw_data = s.read(s.inWaiting()).decode('utf-8')
-        if not 'zGyro' in raw_data:
-            continue
-
-        try:
-            data = ujson.loads(raw_data)
-        except:
-            continue
-        
-
-
-        x_gyro += data['xGyro']
-        y_gyro += data['yGyro']
-        z_gyro += data['zGyro']
-        iter += 1
-
-    x_gyro_offset = x_gyro / iter
-    y_gyro_offset = y_gyro / iter
-    z_gyro_offset = z_gyro / iter
-    '''
-    
-    
     while True:
         raw_data = s.read(s.inWaiting()).decode('utf-8')
-        if not 'zGyro' in raw_data:
+        #raw_data2 = s2.read(s2.inWaiting()).decode('utf-8')
+       
+        if ((not 'zGyro' in raw_data)) :
             continue
         try:
             data = ujson.loads(raw_data)
+            second = ujson.loads(raw_data2)
             for i,num in enumerate(data):
                 data[labels[i]] = round((data[labels[i]]),2)
         except:
             continue
-        '''x_gyro_offset = x_gyro / iter
-        y_gyro_offset = y_gyro / iter
-        z_gyro_offset = z_gyro / iter'''
         
         pitch_direc= -1*sign(data['xAcc'])
         roll_direc = sign(data['zAcc'])
@@ -101,11 +85,12 @@ def main():
         brick.light(Color.GREEN)
         wait(300)
         duration = map(roll,'roll')
-        flick_mag = math.pow((math.pow(data['yGyro'],2)+math.pow(data['zGyro'],2)+math.pow(data['xGyro'],2)),0.5)
+
         #option 2: drumsticks (needs 2 arduinos, 1 to set, 1 to play)
+        flick_mag = math.pow((math.pow(data['yGyro'],2)+math.pow(data['zGyro'],2)+math.pow(data['xGyro'],2)),0.5)
         flick = []
-        
-        while  flick_mag > 70:
+        '''
+        while  flick_mag > 200:
             # Read raw data
             raw_data = s.read(s.inWaiting()).decode('utf-8')
             if not 'zGyro' in raw_data:
@@ -118,22 +103,21 @@ def main():
             flick_mag = math.pow((math.pow(data['yGyro'],2)+math.pow(data['zGyro'],2)+math.pow(data['xGyro'],2)),0.5)
             if flick_mag > 70:
                 flick.append(flick_mag)
-        
+        #print(flick)
+        '''
         mag = 1
         if len(flick)>0:
             length = len(flick)
             mag = int(map(max(flick),'flick'))
-        print(mag)
+        print(drum(roll))
 
         #brick.sound.beep(freq, duration, 1)
-        brick.sound.file(SoundFile.HELLO,mag )
+        brick.sound.file(drum(roll),100)
         #brick.sound.say('words')
         wait(500)
         #print(data)
 
-def drum(roll):
-    for i in len(begin):
-    begin = [0,60,120,180,240,300]
-    end = [60,120,180,240,300,330.360]
-    files = ['Snare.wav']
+
+
+
 main()
