9614b556e6fff58656e967b9aee0eae8b4fe6b0d
Priya Misner <priya.misner@tufts.edu>
2020-02-24 20:25:00 -0500

    update before merge

--- Project4_Blind/Project4_ImagesforBlind/main.py
+++ Project4_Blind/Project4_ImagesforBlind/main.py
@@ -9,79 +9,58 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from pybricks.iodevices import AnalogSensor, UARTDevice
-def colorcheck():
+def colorcheck(side,up,direction):
     if color.color() != Color.WHITE:
-        uart.write('b')
-        print('LETTER')
+        if direction == -1:
+            side = 80 -side
+        fobj.write(str(side)+','+str(up)+'\n')
     else:
         uart.write('a')
 
 brick.sound.beep()
-print("pls but sadder")
 minimotor = Motor(Port.C)
 leftmotor = Motor(Port.A, Direction.CLOCKWISE)
+watch = StopWatch()
+watchside = StopWatch()
 rightmotor= Motor(Port.D, Direction.CLOCKWISE)
 robot = DriveBase(leftmotor,rightmotor, 40, 200)
-#button = TouchSensor(Port.S1)
 color = ColorSensor(Port.S1)
 
-#sense = AnalogSensor(Port.S3, False)
 uart = UARTDevice(Port.S4, 9600, timeout=1000)
 direction = 1
 
 #initial waiting phase:
-
-handshake = "f"
-while uart.waiting() == 0:
-    wait(10)
-print('b4')
-handshake = uart.read(1)
-print('hi',len(handshake))
 holder = b'a'
 motorspeed = 0
 minispeed = 0
+filename = 'letters.csv'
+fobj = open(filename, 'w')
+for up in range(20):
+    print(up)
+    watch.reset()
+    watch.resume()
+    while watch.time() < 200:
+        robot.drive(20, 0)
+    robot.drive(0,0)
+    for side in range(80):
+        watchside.reset()
+        watchside.resume()
+        while watchside.time()<150:
+            minimotor.run(direction*-50)
+            colorcheck(side,up,direction)
+    direction *= -1
 while True:
     colorcheck()
+    direction = 1
     if uart.waiting() != 0:
         holder = uart.read(1).decode("utf-8")
         print(holder)
-        if holder == 'y': #forward
-            motorspeed = 20
-            minispeed = 0
-        elif holder == 'u': #backward
-            motorspeed = -20
-            minispeed = 0
-        elif holder == 's': #left
-            minispeed = -20
-            motorspeed = 0
-        elif holder == 'w': #right
-            minispeed = 20
-            motorspeed = 0
-        elif holder == 'x': # diagonal up right
-            minispeed = 20
-            motorspeed = 20
-        elif holder == 'z': # diagonal up left
-            minispeed = -20
-            motorspeed = 20
-        elif holder == 't': # diagonal down left
-            minispeed = -20
-            motorspeed = -20
-        elif holder == 'v': # diagonal down right
-            minispeed = 20
-            motorspeed = -20
-        else: # q; middle
+        if holder == 'q': #forward
             motorspeed = 0
             minispeed = 0
-        
-        ''' if color.color() != Color.WHITE:
-            uart.write('b')
-            print('LETTER')
-        else:
-            uart.write('a')'''
-   
+
+    #robot.drive(motorspeed,0)
+    #minimotor.run(minispeed)
     
-
-
-    robot.drive(motorspeed,0)
-    minimotor.run(minispeed)
     wait(0.1)
+fobj.close()
--- Project5_MagicConductor/main.py
+++ Project5_MagicConductor/main.py
@@ -12,112 +12,115 @@ from serial import Serial
 import ujson
 import math
 
+
+
 # Write your program here
 ev3 = EV3Brick()
 #ev3.speaker.say('start')
-brick.sound.beep(500,1000,20)
-tare = TouchSensor(Port.S1)
-wait(100)
+ev3.spealer.file('cymbal-01.wav')
+#brick.sound.beep(500,1000,20)
+# tare = TouchSensor(Port.S1)
+# wait(100)
 
-def sign(val):
-    if (val >= 0):
-         return 1 
-    else:
-         return -1
+# def sign(val):
+#     if (val >= 0):
+#          return 1 
+#     else:
+#          return -1
 
-def constrain(val, min, max):
-    if val > max:
-        return max
-    elif val < min:
-        return min 
-    else:
-        return val
+# def constrain(val, min, max):
+#     if val > max:
+#         return max
+#     elif val < min:
+#         return min 
+#     else:
+#         return val
 
-def map(val, mode):
-    if mode =='pitch':
-        val = ((val + 180)/360 )*5000 + 600
-        return val
-    elif mode =='roll':
-        val = ((val + 180)/360 )*3000 + 400
-        return val
-    elif mode=='flick':
-        val = (val/1000)*90
-        return val
+# def map(val, mode):
+#     if mode =='pitch':
+#         val = ((val + 180)/360 )*5000 + 600
+#         return val
+#     elif mode =='roll':
+#         val = ((val + 180)/360 )*3000 + 400
+#         return val
+#     elif mode=='flick':
+#         val = (val/1000)*90
+#         return val
 
-def drum(roll):
-    roll +=270
-    begin = [0,120,240]
-    end = [120,240,360]
-    files = ['AcousticHat-06.wav','AcousticSnare-04.wav','cymbal-01.wav']
-    for i in range(len(begin)):
-        if roll >= begin[i] and roll <end[i]:
-            return(files[i])
+# def drum(roll):
+#     roll +=270
+#     begin = [0,120,240]
+#     end = [120,240,360]
+#     files = ['AcousticHat-06.wav','AcousticSnare-04.wav','cymbal-01.wav']
+#     for i in range(len(begin)):
+#         if roll >= begin[i] and roll <end[i]:
+#             return(files[i])
        
-def main():
-    s = Serial("/dev/ttyACM0", 9600)
-    s2 = Serial("/dev/ttyACM1", 9600)
-    iter = 0
-    labels = ['xAcc', 'yAcc', 'zAcc', 'xGyro', 'yGyro', 'zGyro']
-    x_gyro, y_gyro, z_gyro = 0, 0, 0
-    while True:
-        raw_data = s.read(s.inWaiting()).decode('utf-8')
-        #raw_data2 = s2.read(s2.inWaiting()).decode('utf-8')
+# def main():
+#     s = Serial("/dev/ttyACM0", 9600)
+#     s2 = Serial("/dev/ttyACM1", 9600)
+#     iter = 0
+#     labels = ['xAcc', 'yAcc', 'zAcc', 'xGyro', 'yGyro', 'zGyro']
+#     x_gyro, y_gyro, z_gyro = 0, 0, 0
+#     while True:
+#         raw_data = s.read(s.inWaiting()).decode('utf-8')
+#         #raw_data2 = s2.read(s2.inWaiting()).decode('utf-8')
        
-        if ((not 'zGyro' in raw_data)) :
-            continue
-        try:
-            data = ujson.loads(raw_data)
-            #second = ujson.loads(raw_data2)
-            for i,num in enumerate(data):
-                data[labels[i]] = round((data[labels[i]]),2)
-        except:
-            continue
+#         if ((not 'zGyro' in raw_data)) :
+#             continue
+#         try:
+#             data = ujson.loads(raw_data)
+#             #second = ujson.loads(raw_data2)
+#             for i,num in enumerate(data):
+#                 data[labels[i]] = round((data[labels[i]]),2)
+#         except:
+#             continue
         
-        pitch_direc= -1*sign(data['xAcc'])
-        roll_direc = sign(data['zAcc'])
-        pitch = pitch_direc*math.acos(constrain(data['zAcc'],-1,1))*180/math.pi
-        roll = (roll_direc*math.acos(constrain(data['yAcc'],-1,1))*180/math.pi) -90
+#         pitch_direc= -1*sign(data['xAcc'])
+#         roll_direc = sign(data['zAcc'])
+#         pitch = pitch_direc*math.acos(constrain(data['zAcc'],-1,1))*180/math.pi
+#         roll = (roll_direc*math.acos(constrain(data['yAcc'],-1,1))*180/math.pi) -90
         
-        #option 1 for control: settings
-        freq = map(pitch,'pitch')
-        brick.light(Color.RED)
-        wait(700)
-        brick.light(Color.GREEN)
-        wait(300)
-        duration = map(roll,'roll')
+#         #option 1 for control: settings
+#         freq = map(pitch,'pitch')
+#         brick.light(Color.RED)
+#         wait(700)
+#         brick.light(Color.GREEN)
+#         wait(300)
+#         duration = map(roll,'roll')
 
-        #option 2: drumsticks (needs 2 arduinos, 1 to set, 1 to play)
-        flick_mag = math.pow((math.pow(data['yGyro'],2)+math.pow(data['zGyro'],2)+math.pow(data['xGyro'],2)),0.5)
-        flick = []
+#         #option 2: drumsticks (needs 2 arduinos, 1 to set, 1 to play)
+#         flick_mag = math.pow((math.pow(data['yGyro'],2)+math.pow(data['zGyro'],2)+math.pow(data['xGyro'],2)),0.5)
+#         flick = []
         
-        while  flick_mag > 200:
-            # Read raw data
-            raw_data = s.read(s.inWaiting()).decode('utf-8')
-            if not 'zGyro' in raw_data:
-                continue
-            try:
-                data = ujson.loads(raw_data)
-            except:
-                continue
+#         while  flick_mag > 200:
+#             # Read raw data
+#             raw_data = s.read(s.inWaiting()).decode('utf-8')
+#             if not 'zGyro' in raw_data:
+#                 continue
+#             try:
+#                 data = ujson.loads(raw_data)
+#             except:
+#                 continue
             
-            flick_mag = math.pow((math.pow(data['yGyro'],2)+math.pow(data['zGyro'],2)+math.pow(data['xGyro'],2)),0.5)
-            if flick_mag > 70:
-                flick.append(flick_mag)
-        #print(flick)
+#             flick_mag = math.pow((math.pow(data['yGyro'],2)+math.pow(data['zGyro'],2)+math.pow(data['xGyro'],2)),0.5)
+#             if flick_mag > 70:
+#                 flick.append(flick_mag)
+#         #print(flick)
         
-        mag = 1
-        if len(flick)>0:
-            length = len(flick)
-            mag = int(map(max(flick),'flick'))
-        print(drum(roll))
+#         mag = 1
+#         if len(flick)>0:
+#             length = len(flick)
+#             mag = int(map(max(flick),'flick'))
+#         print(drum(roll))
 
-        #brick.sound.beep(freq, duration, 1)
-        brick.sound.file(drum(roll),100)
-        #brick.sound.say('words')
-        wait(500)
-        #print(data)
-
-
+#         #brick.sound.beep(freq, duration, 1)
+#         brick.sound.file(drum(roll),100)
+#         #brick.sound.say('words')
+#         wait(500)
+#         #print(data)
 
 
-main()
+
+
+# main()
